7f1b73784815518999bae1a957f540fd549d2caa
Afzal Ansari <afzal442@gmail.com>
2021-11-14 20:54:42 +0530

    minor changes

--- web-api/main.go
+++ web-api/main.go
@@ -6,22 +6,6 @@ import (
 
 func main() {
 
-	// conn, err := grpc.Dial("localhost:9092", grpc.WithInsecure())
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// // create client
-	// cc := pb.NewCurrencyClient(conn)
-
-	// // create database instance
-	// db := data.NewProductsDB(cc, l)
-
-	// // create the handlers
-	// ph := handlers.NewProducts(l, v, db)
-
-	// defer conn.Close()
-
 	// Initialize the routes
 	r := routes.InitializeRoutes()
 
